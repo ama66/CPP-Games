dd7ff1bfa34e8764200a13a9bf5fc3b1010652b6
Ammar Abdilghanie <ama66@cornell.edu>
2023-02-07 23:20:53 -0600

    Create Pawn.cpp

---
+++ Pawn.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include "Pawn.h"
+using namespace std; 
+
+void Pawn::BeginPlay() {
+
+	cout << "Pawn BeginPlay() called . \n\n";
+
+}
+
+
+void Pawn::PawnFunction() {
+	cout << "Pawn Function Called. \n\n";
+}
+
