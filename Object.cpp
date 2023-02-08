bef6555534ea77d3cb9a47b3b4cef9b370affd74
Ammar Abdilghanie <ama66@cornell.edu>
2023-02-07 23:22:41 -0600

    Create Object.cpp

---
+++ Object.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+# include "Object.h"
+using namespace std;
+
+
+void Object::BeginPlay() {
+
+	cout << "Object BeginPlay() called . \n\n";
+
+}
+
+void Object::ObjectFunction()
+{
+	cout << "Object Function Called. \n\n";
+}
