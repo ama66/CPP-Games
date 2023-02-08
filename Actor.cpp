24b346dea7ff8c97ff10483338608cd68e455caa
Ammar Abdilghanie <ama66@cornell.edu>
2023-02-07 23:21:39 -0600

    Create Actor.cpp

---
+++ Actor.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Actor.h"
+using namespace std;
+
+
+void Actor::BeginPlay() {
+
+	cout << "Actor BeginPlay() called . \n\n";
+
+}
+
+void Actor::ActorFunction()
+{
+	cout << " Actor Function Called. \n\n";
+}
+
