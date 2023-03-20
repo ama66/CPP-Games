e228b20039a530f11bc44e9f4f507ccfa31ea422
Ammar Abdilghanie <ama66@cornell.edu>
2023-03-20 09:34:33 -0500

    Update functionsbasic.cpp

--- ModernCPP/functionsbasic.cpp
+++ ModernCPP/functionsbasic.cpp
@@ -1,4 +1,43 @@
 #include <iostream>
+#include <array>
+
+
+//double sum( double scores[], size_t count){ this array scores decay into a pointer so cannot use sizeof(scores)
+double sum (double* scores , size_t count){
+
+    double score_sum{};
+
+   // std::cout << "sizeof(scores)  : " << sizeof(scores) << std::endl;
+   // std::cout << "sizeof(int*) : " << sizeof (int *) << std::endl;
+   //size_t arr_size = std::size(scores);
+
+   for(size_t i{0} ; i < count ; ++i){
+       score_sum += scores[i];
+   }
+    return score_sum;
+}
+
+
+
+int main(){
+    double my_scores[]{10.5,34.3,4.8,6.5};
+    //std::cout << "sizeof(my_scores) : " << sizeof(my_scores) << std::endl;
+   
+
+   double result = sum(my_scores,std::size(my_scores));
+   std::cout << "result : " << result << std::endl;
+ 
+    return 0;
+}
+
+
+
+
+
+
+
+###############################################################################
+#include <iostream>
 
 
 double sum ( double scores[], size_t count){
