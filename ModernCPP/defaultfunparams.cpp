c0dccdd5c54bdc2093f9d3fe53164bf3c9ac0796
Ammar Abdilghanie <ama66@cornell.edu>
2023-03-20 17:50:46 -0500

    Update defaultfunparams.cpp

--- ModernCPP/defaultfunparams.cpp
+++ ModernCPP/defaultfunparams.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+//default arguments are only used in the declaration. compiler will error out if used in implementation 
 void compute( int age = 27 , double weight = 72.4, double distance = 12.5);
 void greet_teacher(std::string_view name_sv = "teacher" , int homeworks =12,
     int exams = 4, double pass_rate = 0.5, std::string_view first_dpmt = "Computer Sce");
