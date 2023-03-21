8743351816c02a6d48ae43ccb490aa618e518659
Ammar Abdilghanie <ama66@cornell.edu>
2023-03-20 22:40:36 -0500

    Update functiontypeConversion.cpp

--- ModernCPP/functiontypeConversion.cpp
+++ ModernCPP/functiontypeConversion.cpp
@@ -58,4 +58,23 @@ void print_out(const int & value){
     std::cout << "value : " << value << std::endl;
 }
 
+//  similarly cannot pass a pointer to a double here so be careful! 
+
+
+#include <iostream>
+
+void print_sum(int* param1, int* param2){
+	std::cout << "sum : " << (*param1 + *param2) << std::endl;
+}
+
+int main(){
+
+    int a{3};
+    int b{12};
+
+    print_sum(&a,&b);
+   
+    return 0;
+}
+
 
