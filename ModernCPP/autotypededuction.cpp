88ea7978272b3b1d9c9f56488d4991c0c2478782
Ammar Abdilghanie <ama66@cornell.edu>
2023-03-24 18:02:30 -0500

    Update autotypededuction.cpp

--- ModernCPP/autotypededuction.cpp
+++ ModernCPP/autotypededuction.cpp
@@ -96,3 +96,46 @@ int main(){
    
     return 0;
 }
+
+
+//auto& for reference return otherwise you get a copy 
+
+
+#include <iostream>
+
+
+auto& max(int& a, int& b){
+    if(a>b){
+        return a;
+    }else{
+        return b; // Will return a copy.
+    }
+}
+
+int main(){
+
+
+    int x{10};
+    int y{45};
+
+	int& result = max(x,y); // Error : Can not treat return value as a reference
+							// It's jut a bare separate variable with a value inside.
+							
+	//int result = max(x,y); // A copy of the max is returned
+	
+	++ result;
+	
+	std::cout << "x :" << x << std::endl; // 10
+	std::cout << "y :" << y << std::endl; // 46
+	std::cout << "result : " << result << std::endl; // 46
+   
+    return 0;
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+P
+
+
+
